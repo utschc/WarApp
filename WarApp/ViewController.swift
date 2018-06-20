c69ac0a6ac524391fcb034afc455724ec201690b
utschc <utschcharlie@gmail.com>
2018-06-20 11:15:47 -0400

    War App

--- WarApp/ViewController.swift
+++ WarApp/ViewController.swift
@@ -9,17 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    var leftScore = 0
+    var rightScore = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func dealTapped(_ sender: Any) {
+        
+        let leftRandomNumber = arc4random_uniform(13) + 2
+        // print("left random number is: \(leftRandomNumber)")
+        let rightRandomNumber = arc4random_uniform(13) + 2
+        // print("right Random number is: \(rightRandomNumber)")
+        
+        leftImageView.image = UIImage(named: "card\(leftRandomNumber)")
+        rightImageView.image = UIImage(named: "card\(rightRandomNumber)")
+        
+        if leftRandomNumber > rightRandomNumber{
+            leftScore += 1
+            leftScoreLabel.text = String(leftScore)
+        }
+        else if leftRandomNumber < rightRandomNumber{
+            rightScore += 1
+            rightScoreLabel.text = String(rightScore)
+        }
+        else{
+            
+            
+        }
+    }
+    
+    
 }
 
